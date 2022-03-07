43853d9322941640ec6df152552c803370435d7b
LorelayFlorescu <rainbowg03@gmail.com>
2022-03-07 12:53:33 +0100

    checkpoint 5
    acabo de hacerla porque no tenía nada nuevo

--- wordle.py
+++ wordle.py
@@ -43,7 +43,7 @@ def compare_words(word, secret):
     return same_position, same_letter
 
 
-def print_word():
+def print_word(word, same_position, same_letter):
     """Dada una palabra, una lista same_position y otra lista same_letter, esta función creará un string donde aparezcan en mayúsculas las letras de la palabra que ocupen las posiciones de same_position, en minúsculas las letras de la palabra que ocupen las posiciones de same_letter y un guión (-) en el resto de posiciones
     Args:
       word: Una palabra. Ej. "CAMPO"
@@ -52,18 +52,23 @@ def print_word():
     Returns:
       transformed: La palabra aplicando las transformaciones. En el caso anterior: "Cam--"
     """
-    same_position = [0]
+    same_position = [3]
     same_letter = [1, 2]
     word = "CAMPO"
     transformed = ""
-    tupla = same_position, same_letter
+
     for i in range(len(word)):
+      transformed = transformed + "-"
+      print(transformed)
       for j in same_position:
         if(i==j):
-          transformed = transformed + word[i]
-        else: 
-          transformed = transformed + word[i].lower()
+          transformed = transformed[:j] + word[i] + transformed[j+1:]
+      for j in same_letter:
+        if(i==j):
+          transformed = transformed[:j] + word[i].lower() + transformed[j+1:]
 
+    print(transformed)
+    return transformed
     
 def choose_secret_advanced():
     """Dado un nombre de fichero, esta función filtra solo las palabras de 5 letras que no tienen acentos (á,é,í,ó,ú). De estas palabras, la función devuelve una lista de 15 aleatorias no repetidas y una de estas 15, se selecciona aleatoriamente como palabra secret.
